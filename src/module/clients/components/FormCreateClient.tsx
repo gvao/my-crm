ce4db7692e3d07843b483ddda8da58f15bc40e66
Yuri Galvao <yuri.gvao@gmail.com>
2024-06-26 23:49:11 -0300

    created FormCreateClient component

---
+++ src/module/clients/components/FormCreateClient.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { ChangeEventHandler, FormEventHandler, useContext, useState } from "react"
+import { useClientContext } from "../context"
+
+export default function FormCreateClient() {
+    const { createClient } = useClientContext()
+    const [state, setState] = useState<State>({
+        name: '',
+        contact: '',
+    })
+
+    const onChange: ChangeEventHandler<HTMLInputElement> = ({ target: { id, value } }) => setState(state => ({ ...state, [id]: value }))
+    const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault()
+        createClient(state)
+        console.log(`submit`)
+    }
+    return (
+        <form onSubmit={onSubmit} >
+            <h2>Criar novo cliente</h2>
+            <input autoFocus type="text" name="name" id="name" value={state.name} onChange={onChange} />
+            <input type="number" name="contact" id="contact" value={state.contact} onChange={onChange} />
+            <input type="submit" value="Cadastrar" />
+        </form>
+    )
+}
+
+interface State {
+    name: string,
+    contact: string
+}
