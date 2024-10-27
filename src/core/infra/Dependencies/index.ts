55ae6dbe193a49da4e1254ff830eb8fee67bd9fa
Yuri Galvao <yuri.gvao@gmail.com>
2024-10-27 20:01:00 -0300

    insert updateClient and getClientById in dependencies

--- src/core/infra/Dependencies/index.ts
+++ src/core/infra/Dependencies/index.ts
@@ -5,6 +5,8 @@ import ClientRepositoryInMemory from "../repositories/ClientRepositoryInMemory"
 import DeleteClient from "@/core/application/useCases/DeleteClient"
 import CreateClient from "../../application/useCases/CreateClient"
 import GetClients from "../../application/useCases/GetClients"
+import UpdateClient from "@/core/application/useCases/UpdateClient"
+import GetClientById from "@/core/application/useCases/GetClientById"
 
 const Dependencies = new DependenciesRepository()
 
@@ -13,9 +15,13 @@ const clientRepository = new ClientRepositoryInMemory()
 const createClient = new CreateClient(clientRepository)
 const getClients = new GetClients(clientRepository)
 const deleteClient = new DeleteClient(clientRepository)
+const updateClient = new UpdateClient(clientRepository)
+const getClientById = new GetClientById(clientRepository)
 
 Dependencies.cadasterDependency('createClient', createClient)
 Dependencies.cadasterDependency('getClients', getClients)
 Dependencies.cadasterDependency('deleteClient', deleteClient)
+Dependencies.cadasterDependency('updateClient', updateClient)
+Dependencies.cadasterDependency('getClientById', getClientById)
 
 export default Dependencies
