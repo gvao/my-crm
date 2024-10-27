725425f5c46d54634c1e7b962b99996bc40b5edd
Yuri Galvao <yuri.gvao@gmail.com>
2024-10-27 20:02:49 -0300

    created getById method in ClientRepository and update save method

--- src/core/infra/repositories/ClientRepositoryInMemory.ts
+++ src/core/infra/repositories/ClientRepositoryInMemory.ts
@@ -1,8 +1,13 @@
-import { GetAllRepository, RemoveItemRepository, SaveRepository } from "@/core/application/repository/interface";
+import { GetAllRepository, RemoveItemRepository, SaveRepository, getItemById } from "@/core/application/repository/interface";
 import IClient from "@/core/Domain/Client/interface";
 
 export default class ClientRepositoryInMemory implements IClientRepository {
     clients: IClient[] = []
+
+    async getById(id: string): Promise<IClient | undefined> {
+        const clientFound = this.clients.find(client => client.id === id);
+        return clientFound
+    }
     async remove(id: string): Promise<void> {
         const index = this.clients.findIndex(client => client.id === id)
         this.clients.splice(index, 1)
@@ -11,9 +16,18 @@ export default class ClientRepositoryInMemory implements IClientRepository {
         return this.clients
     }
     async save(client: IClient): Promise<void> {
-        this.clients.push(client)
+        const index = this.clients.findIndex(clientRepository => clientRepository.id === client.id)
+        if (index < 0) await this.addClient(client)
+        this.clients.splice(index, 1, client)
     }
 
+    private async addClient(client: IClient): Promise<void> {
+        this.clients.push(client)
+    }
 }
 
-type IClientRepository = SaveRepository<IClient> & GetAllRepository<IClient[]> & RemoveItemRepository<IClient>
+type IClientRepository = {}
+    & SaveRepository<IClient>
+    & GetAllRepository<IClient[]>
+    & RemoveItemRepository<IClient>
+    & getItemById<IClient>
