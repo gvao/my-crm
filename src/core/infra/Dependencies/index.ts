0c8eb23ecba9a2d7098f5d2756b8466bba6410ac
Yuri Galvao <yuri.gvao@gmail.com>
2024-10-28 00:53:15 -0300

    initializer with fake clients

--- src/core/infra/Dependencies/index.ts
+++ src/core/infra/Dependencies/index.ts
@@ -7,6 +7,7 @@ import CreateClient from "../../application/useCases/CreateClient"
 import GetClients from "../../application/useCases/GetClients"
 import UpdateClient from "@/core/application/useCases/UpdateClient"
 import GetClientById from "@/core/application/useCases/GetClientById"
+import Client from "@/core/Domain/Client"
 
 const Dependencies = new DependenciesRepository()
 
@@ -16,7 +17,16 @@ const createClient = new CreateClient(clientRepository)
 const getClients = new GetClients(clientRepository)
 const deleteClient = new DeleteClient(clientRepository)
 const updateClient = new UpdateClient(clientRepository)
-const getClientById = new GetClientById(clientRepository)
+const getClientById = new GetClientById(clientRepository);
+
+(async () => {
+    const clientsQuantity = 4
+
+    for(let i = 0; i < clientsQuantity; i++) {
+        const client = Client.create({ name: `client ${i}`, contact: 1234567890 + i })
+        clientRepository.save(client)
+    }
+})()
 
 Dependencies.cadasterDependency('createClient', createClient)
 Dependencies.cadasterDependency('getClients', getClients)
