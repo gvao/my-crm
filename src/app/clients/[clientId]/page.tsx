4cd587aec341e10e47b04045d8281f38d8053c67
Yuri Galvao <yuri.gvao@gmail.com>
2024-10-28 00:57:41 -0300

    creates Client page for Details

---
+++ src/app/clients/[clientId]/page.tsx
@@ -0,0 +1,15 @@
+import Title from "@/module/shared/components/Title"
+import DetailsClient from "../components/DetailsClient"
+import DeleteClient from "../components/DeleteClient"
+
+const ClientDetailsPage = async ({ params, ...props }: { params: { clientId: string } }) => {
+    const { clientId } = await (params)
+    return <>
+        <Title>Detalhes</Title>
+        <DetailsClient clientId={clientId} />
+        <DeleteClient clientId={clientId} />
+    </>
+}
+
+
+export default ClientDetailsPage
