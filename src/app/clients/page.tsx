57331863d9d97816eb85ad5a7fdbe426bc975d5f
Yuri Galvao <yuri.gvao@gmail.com>
2024-10-31 21:07:18 -0300

    remove "revalidate"

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -2,8 +2,6 @@ import FormClient from './components/FormClient'
 import Title from '@/module/shared/components/Title'
 import ListClients from './components/ListClients'
 
-
-export const revalidate = 30
 export default async function ClientsPage() {
 
     return (<>
