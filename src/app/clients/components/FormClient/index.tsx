557b9785be52b6de11400b43421dae6cc34613d4
Yuri Galvao <yuri.gvao@gmail.com>
2024-10-31 22:58:03 -0300

    Updated style FormClient component

--- src/app/clients/components/FormClient/index.tsx
+++ src/app/clients/components/FormClient/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation"
 import { FormEvent } from "react"
 import styles from './styles.module.css'
 import Button from "../Button"
+import Input from "@/module/shared/components/Input"
 
 export default function FormClient() {
     const gateway = ClientsGateway()
@@ -13,7 +14,7 @@ export default function FormClient() {
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
         const form = e.target as HTMLFormElement
-        const formData = new FormData(e.target as HTMLFormElement)
+        const formData = new FormData(form)
         const name = formData.get('name') as string
         const contact = formData.get('contact') as string
 
@@ -25,14 +26,14 @@ export default function FormClient() {
     return (<>
         <div className={styles.wrapper} >
 
-            <h2>Create client</h2>
+            <h2>Novo cliente</h2>
 
             <form onSubmit={handleSubmit} className={styles.form} >
-                <input className={styles.input} type="text" name="name" id="name" autoFocus />
-                <input className={styles.input} type="number" name="contact" id="contact" />
+                <Input name="name" id="name" autoFocus placeholder="Nome do cliente" label="Nome cliente" />
+                <Input placeholder="(21) 99999-9999" label="Seu contato" type="number" name="contact" id="contact" />
                 <Button type="submit" >Criar</Button>
-
             </form>
         </div>
     </>)
 }
+
